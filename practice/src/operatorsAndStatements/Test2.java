bbbe7f6dc3dd94e3d97e58871a03247604552753
Azua <amado.azua@cgi.com>
2022-03-16 16:24:01 -0500

    Operators and Statements review

---
+++ practice/src/operatorsAndStatements/Test2.java
@@ -0,0 +1,14 @@
+package operatorsAndStatements;
+
+/*
+    The result for the following code is -100. There are 2 operators involved in this. Unary minus
+        and Postfix operator. The value of a is changed to 101 after the sout on line 12, and because
+        there is a unary minus we have the -100 print out on the console.
+*/
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println(-a++);
+    }
+}
