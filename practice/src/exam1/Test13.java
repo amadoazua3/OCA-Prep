c166a1f7c35327da3b778765e064f66ff82fda8f
Azua <amado.azua3@gmail.com>
2022-03-22 09:43:41 -0500

    OCA prep

---
+++ practice/src/exam1/Test13.java
@@ -0,0 +1,20 @@
+package exam1;
+
+/*
+    main method's parameter variable name is "args" and it is a local to main method.
+
+    So, same name "args" can't be used directly within the curly brackets of main
+    method.
+
+    short[] args = new short[]{50, 50}; gives compilation error for using same name for
+    local variable.
+*/
+
+public class Test13 {
+    public static void main(String[] args) {
+        short [] args = new short[]{50, 50};
+        args[0] = 5;
+        args[1] = 10;
+        System.out.println("[" + args[0] + ", " + args[1] + "]");
+    }
+}
