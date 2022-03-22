f5daa08cf0195cb2a0497cc3320f9a86d525d7e5
Azua <amado.azua3@gmail.com>
2022-03-22 10:23:35 -0500

    OCA prep

--- practice/src/exam1/Test13.java
+++ practice/src/exam1/Test13.java
@@ -12,9 +12,9 @@ package exam1;
 
 public class Test13 {
     public static void main(String[] args) {
-        short [] args = new short[]{50, 50};
-        args[0] = 5;
-        args[1] = 10;
+//        short [] args = new short[]{50, 50};
+//        args[0] = 5;
+//        args[1] = 10;
         System.out.println("[" + args[0] + ", " + args[1] + "]");
     }
 }
---
+++ practice/src/exam1/Test18.java
@@ -0,0 +1,31 @@
+package exam1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    list1 --> [A, D],
+    list2 --> [B, C],
+
+    list1.addAll(1, list2); is almost equal to list1.add(1, [B, C]); => Inserts B at index 1, C
+    takes index 2 and D is moved to index 3. list1 --> [A, B, C, D]
+*/
+
+public class Test18 {
+
+    public static void main(String[] args) {
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("A");
+        list1.add("D");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("B");
+        list2.add("C");
+
+        list1.addAll(1, list2);
+
+        System.out.println(list1);
+
+    }
+}
