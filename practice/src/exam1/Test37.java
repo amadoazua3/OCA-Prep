8b22fab4481e8daf81393f310dba385a42a2481f
Azua <amado.azua3@gmail.com>
2022-03-24 14:55:28 -0500

    OCA prep

---
+++ practice/src/exam1/Test37.java
@@ -0,0 +1,23 @@
+package exam1;
+
+import java.time.Period;
+
+/*
+    Period.of(0, 0, 0); is equivalent to Period.ZERO. ZERO period is displayed as P0D,
+    other than that, Period components (year, month, day) with 0 values are ignored.
+
+    toString()'s result starts with P, and for non-zero year, Y is appended; for non-zero
+    month, M is appended; and for non-zero day, D is appended. P,Y,M and D are in upper case.
+
+    NOTE: Period.parse(CharSequence) method accepts the String parameter in
+    "PnYnMnD" format, over here P,Y,M and D can be in any case.
+*/
+
+public class Test37 {
+
+    public static void main(String[] args) {
+
+        Period period = Period.of(0, 0, 0);
+        System.out.println(period);
+    }
+}
