549c401c6a9a0fbebe4311c5d2a31f905f471eb7
Azua <amado.azua3@gmail.com>
2022-03-24 14:31:16 -0500

    OCA prep

---
+++ practice/src/exam1/Test32.java
@@ -0,0 +1,22 @@
+package exam1;
+
+/*
+    As do-while loop executes at least once, hence none of the code is unreachable in
+    this case.
+
+    Java runtime prints 100 to the console, then it checks boolean expression, which is
+    false.
+
+    Hence control goes out of do-while block. Java runtime executes 2nd
+    System.out.println statement to print "Bye" on to the console.
+*/
+
+public class Test32 {
+    public static void main(String[] args) {
+
+        do {
+            System.out.println(100);
+        } while (false);
+        System.out.println("Bye");
+    }
+}
