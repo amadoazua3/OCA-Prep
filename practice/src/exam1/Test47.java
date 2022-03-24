62fa1e3d40aa902f9c6476f0b1c43050d8c8b404
Azua <amado.azua3@gmail.com>
2022-03-24 16:02:33 -0500

    OCA prep

---
+++ practice/src/exam1/Test47.java
@@ -0,0 +1,16 @@
+package exam1;
+
+/*
+int variable can easily be assigned to double type but double [] and int [] are not
+compatible. In fact, both are siblings and can't be assigned to each other, so Line 13
+causes compilation failure.
+*/
+
+public class Test47 {
+
+    public static void main(String[] args) {
+
+        double [] arr = new int[2];
+        System.out.println(arr[0]);
+    }
+}
