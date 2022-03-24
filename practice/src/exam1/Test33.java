53277b02507f97b42d1577ab0dfe67c5000d6a9f
Azua <amado.azua3@gmail.com>
2022-03-24 14:34:16 -0500

    OCA prep

---
+++ practice/src/exam1/Test33.java
@@ -0,0 +1,23 @@
+package exam1;
+
+/*
+    toString() method defined in StringBuilder class doesn't use String literal rather uses
+    the constructor of String class to create the instance of String class.
+
+    So both s1 and s2 refer to different String instances even though their contents are
+    same. s1 == s2 returns false.
+*/
+
+public class Test33 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Java");
+        String s1 = sb.toString();
+        String s2 = sb.toString();
+
+        System.out.println(s1 == s2
+        );
+    }
+
+}
