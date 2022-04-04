5c4b1d590589a904ad926d926b0c04e6620c787e
Azua <amado.azua3@gmail.com>
2022-04-04 15:18:55 -0500

    OCA prep

---
+++ practice/src/exam2/Test34.java
@@ -0,0 +1,42 @@
+package exam2;
+
+/*
+    case values must evaluate to the same type / compatible type as the switch
+    expression can use.
+
+    switch expression can accept following:
+
+    char or Character,
+
+    byte or Byte,
+
+    short or Short,
+
+    int or Integer,
+
+    An enum only from Java 6,
+
+    A String expression only from Java 7.
+
+    In this case, switch expression [switch (score)] is of int type.
+
+    But case expressions, score < 70 and score >= 70 are of boolean type and hence
+    compilation error.
+*/
+
+public class Test34 {
+
+//    public static void main(String[] args) {
+//        int score = 60;
+//        switch (score) {
+//            default:
+//                System.out.println("Not a valid score");
+//            case score < 70:
+//                System.out.println("Failed");
+//                break;
+//            case score >= 70:
+//                System.out.println("Passed");
+//                break;
+//        }
+//    }
+}
