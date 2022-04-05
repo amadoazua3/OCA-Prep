031bf8393302a71441381527ddf7444c4e5b0409
Azua <amado.azua3@gmail.com>
2022-04-05 09:45:20 -0500

    OCA prep

--- practice/src/exam2/Test36.java
+++ practice/src/exam2/Test36.java
@@ -19,29 +19,29 @@ import java.util.List;
     the list. Remaining 3 list elements are printed in the insertion order.
 */
 
-class Student {
-    private String name;
-    private int age;
-
-    Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String toString() {
-        return "Student[" + name + ", " + age + "]";
-    }
-
-    public boolean equals(Object obj) {
-        if(obj instanceof Student) {
-            Student stud = (Student)obj;
-            if(this.name.equals(stud.name) && this.age == stud.age) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+//class Student {
+//    private String name;
+//    private int age;
+//
+//    Student(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public String toString() {
+//        return "Student[" + name + ", " + age + "]";
+//    }
+//
+//    public boolean equals(Object obj) {
+//        if(obj instanceof Student) {
+//            Student stud = (Student)obj;
+//            if(this.name.equals(stud.name) && this.age == stud.age) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
 
 public class Test36 {
 
---
+++ practice/src/exam2/Test59.java
@@ -0,0 +1,31 @@
+package exam2;
+
+/*
+    A constructor can call another constructor by using this(...) and not the constructor
+    name.
+
+    Hence Student("James", 25); causes compilation error.
+*/
+
+//class Student {
+//    String name;
+//    int age;
+//
+//    Student() {
+//        Student("James", 25);
+//    }
+//
+//    Student(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//}
+//
+//
+//public class Test59 {
+//
+//    public static void main(String[] args) {
+//        Student s = new Student();
+//        System.out.println(s.name + ":" + s.age);
+//    }
+//}
