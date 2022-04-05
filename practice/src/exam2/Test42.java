16765c8594384045e8b0bb65347910f0c0be5550
Azua <amado.azua3@gmail.com>
2022-04-05 08:37:35 -0500

    OCA prep

---
+++ practice/src/exam2/Test42.java
@@ -0,0 +1,17 @@
+package exam2;
+
+/*
+Variable arr refers to an array object of String of 7 elements.
+
+Variable arr contains the memory address of String array object.
+
+arr is of reference type, hence it prints some String Containing @ symbol.
+*/
+
+public class Test42 {
+
+    public static void main(String[] args) {
+        String [] arr = new String[7];
+        System.out.println(arr);
+    }
+}
