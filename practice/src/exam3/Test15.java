2e423f705d25839968bcd58757c1bffe027a4c19
Azua <amado.azua3@gmail.com>
2022-04-08 09:29:21 -0500

    OCA prep

---
+++ practice/src/exam3/Test15.java
@@ -0,0 +1,24 @@
+package exam3;
+
+/*
+switch can accept primitive types: byte, short, int, char; wrapper types: Byte, Short,
+Integer, Character; String and enums.
+
+switch(b) causes compilation failure as b is of Boolean type.
+*/
+
+public class Test15 {
+
+    public static void main(String[] args) {
+        Boolean b = new Boolean("tRUe");
+//        switch(b) {
+//            case true:
+//                System.out.println("ONE");
+//            case false:
+//                System.out.println("TWO");
+//            default:
+//                System.out.println("THREE");
+//        }
+    }
+
+}
