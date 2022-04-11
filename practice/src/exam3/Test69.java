e3bbc175c8a35df311b7a18d87e40a611d6f5862
Azua <amado.azua3@gmail.com>
2022-04-11 10:52:46 -0500

    OCA prep

---
+++ practice/src/exam3/Test69.java
@@ -0,0 +1,27 @@
+package exam3;
+
+/*
+    Method m1() throws an instance of ArithmeticException and method m1() doesn't
+    handle it, so it forwards the exception to calling method main.
+
+    Method main doesn't handle ArithmeticException so it forwards it to JVM, but
+    just before that finally block is executed. This prints A on to the console.
+
+    After that JVM prints the stack trace and terminates the program abruptly.
+*/
+
+public class Test69 {
+
+    private static void m1() {
+        System.out.println(1/0);
+    }
+
+    public static void main(String[] args) {
+        try {
+            m1();
+        } finally {
+            System.out.println("A");
+        }
+    }
+
+}
