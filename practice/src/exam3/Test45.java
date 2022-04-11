9b36bffde2d9eb0ffda4f6d4c828cedd16fd1aeb
Azua <amado.azua3@gmail.com>
2022-04-11 09:32:35 -0500

    OCA prep

---
+++ practice/src/exam3/Test45.java
@@ -0,0 +1,57 @@
+package exam3;
+
+/*
+In java, it is allowed to put multiple statements on one line. E.g. below code is legal:
+
+public class Test {
+    public static void main(String [] args) {
+        String symbol = "!";System.out.print("Hello ");System.out.print("World");System.out.println(symbol);
+    }
+}
+
+
+Above code is similar to:
+
+public class Test {
+    public static void main(String [] args) {
+        String symbol = "!";
+        System.out.print("Hello ");
+        System.out.print("World");
+        System.out.println(symbol);
+    }
+}
+
+
+Empty statements (just the semicolon) are also allowed in java, therefore below code is also legal:
+
+public class Test {
+    public static void main(String [] args) {
+        System.out.println("Hello");
+        ;
+        ;
+        ;
+        ;
+        ;
+        ;
+        ;
+        ;
+    }
+}
+
+
+As shown above, java statements (including empty statements) can be placed on one line, therefore below code is legal:
+
+public class Test {
+    public static void main(String [] args) {
+        System.out.println("Hello");;;;;;;;;
+    }
+}
+*/
+
+public class Test45 {
+
+    public static void main(String [] args) {
+        System.out.println("Hello");;;;;;;;;
+    }
+
+}
