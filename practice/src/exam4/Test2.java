0584e5d0c3626681302116c8b1baabbb0edeb747
Azua <amado.azua3@gmail.com>
2022-04-15 08:24:15 -0500

    OCA prep

---
+++ practice/src/exam4/Test2.java
@@ -0,0 +1,19 @@
+package exam4;
+
+/*
+    127 + 21 = 148 = 00000000 00000000 00000000 10010100
+
+    Above binary number is +ve, as left most bit is 0.
+
+    Same binary number after type-casting to byte: 10010100,
+    -ve number as left most bit is 1.
+
+    10010100 = -108.
+*/
+
+public class Test2 {
+    public static void main(String[] args) {
+        byte b1 = (byte) (127 + 21);
+        System.out.println(b1);
+    }
+}
