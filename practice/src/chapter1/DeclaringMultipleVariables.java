bd94843cec3ac20376a4e16ce59b584bd6ef7f7e
Amado Azua III <amado.azua3@gmail.com>
2022-07-05 22:35:13 -0500

    Declaring multiple variables

---
+++ practice/src/chapter1/DeclaringMultipleVariables.java
@@ -0,0 +1,24 @@
+package chapter1;
+
+public class DeclaringMultipleVariables {
+
+    public static void main(String[] args) {
+
+        String s1, s2;
+        String s3 = "yes", s4 = "no";
+
+
+        int i1, i2, i3 = 0;
+
+
+        int num, String value;      // DOES NOT COMPILE
+
+        boolean b1, b2;             // Valid, Compiles
+        String s1 = "1", s2;        // DOES NOT COMPILE
+        double d1, double d2;       // DOES NOT COMPILE
+        int i1; int i2;             // DOES NOT COMPILE
+        int i3; i4;                 // DOES NOT COMPILE
+
+    }
+
+}
