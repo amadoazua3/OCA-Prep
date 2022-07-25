3a30bc5a3dad729a4e05b394ad3b6d748a0b5e21
Amado Azua III <amado.azua3@gmail.com>
2022-07-25 17:35:21 -0500

    Arithmetic Operators

--- practice/src/chapter2/ArithmeticOperators.java
+++ practice/src/chapter2/ArithmeticOperators.java
@@ -8,5 +8,17 @@ public class ArithmeticOperators {
 
         System.out.println(x);
 
+        System.out.println(9 / 3);      // Outputs 3
+        System.out.println(9 % 3);      // Outputs 0
+
+        System.out.println(10 / 3);     // Outputs 3
+        System.out.println(10 % 3);     // Outputs 1
+
+        System.out.println(11 / 3);     // Outputs 3
+        System.out.println(11 % 3);     // Outputs 2
+
+        System.out.println(12 / 3);     // Outputs 4
+        System.out.println(12 % 3);     // Outputs 0
+
     }
 }
